da6c7cb3edb89eabbb8d1fc5b058b313c0c8763d
sabbir ahmmed <ahsabbir103@gmail.com>
2023-08-11 05:02:19 +0600

    media streaming issue

--- forge.config.ts
+++ forge.config.ts
@@ -2,7 +2,7 @@ import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
+// import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -14,11 +14,12 @@ const config: ForgeConfig = {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerDeb({})], // , new MakerRpm({})
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
       mainConfig,
+      devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
       renderer: {
         config: rendererConfig,
         entryPoints: [
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,80 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { Peer } from 'peerjs';
+import {getInterfaces} from 'net';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('peer-connect');
+    const remoteVideoRef = document.getElementById('remote-video');
+    let peer:any;
+
+    button.addEventListener('click', async () => {
+        peer = new Peer();
+
+        const remoteId = peer.on('open', (peerId:string) => {
+            return peerId;
+        });
+
+        console.log("🚀 ~ file: preload.ts:15 ~ peer.on ~ remoteId:", remoteId)
+
+        const interfaces = getInterfaces();
+
+        console.log("🚀 ~ file: preload.ts:15 ~ peer.on ~ peer", interfaces)
+
+        // navigator.mediaDevices.getDisplayMedia({ video: true, audio: false }).then((mediaStream) => {
+        //     const call = peer.call('test', mediaStream);
+        //     // console.log("🚀 ~ file: preload.ts:15 ~ peer.on ~ call:", call)
+        // });
+        
+        // peer.on('open', (peerId:string) => {
+        //     console.log('My peer ID is:', peerId);
+
+        //     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+        //         const call = peer.call(peerId, stream);
+        //         console.log("🚀 ~ file: preload.ts:15 ~ peer.on ~ call:", call)
+        //     //     if (localVideoRef instanceof HTMLMediaElement) {
+        //     //         localVideoRef.srcObject = stream;
+        //     //         // localVideoRef.play();
+        //     //     } else {
+        //     //         console.error('localVideoRef is not an HTMLMediaElement', localVideoRef);
+        //     //     }
+        //     // }, (err) => {
+        //     //     console.log('Failed to get local stream', err);
+        //     });
+        // });
+
+
+
+
+
+            // const call = peer.call(peerId, null, { video: true, audio: true });
+            // console.log("🚀 ~ file: preload.ts:15 ~ peer.on ~ call:", call)
+
+            // call.on('stream', (remoteStream:any) => {
+            //     console.log("🚀 ~ file: preload.ts:17 ~ call.on ~ remoteStream:", remoteStream)
+            //     if (remoteVideoRef instanceof HTMLMediaElement) {
+            //         remoteVideoRef.srcObject = remoteStream;
+            //         // remoteVideoRef.play();
+            //     } else {
+            //         console.error('remoteVideoRef is not an HTMLMediaElement', remoteVideoRef);
+            //     }
+            // });
+        // });
+
+        // peer.on('call', (call:any) => {
+        //     console.log("🚀 ~ file: preload.ts:32 ~ peer.on ~ call", call)
+        //     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        //         .then((stream) => {
+        //             call.answer(stream);
+        //             console.log("🚀 ~ file: preload.ts:50 ~ .then ~ stream:", stream)
+        //             call.on('stream', (remoteStream:any) => {
+        //                 if (remoteVideoRef instanceof HTMLMediaElement) {
+        //                     remoteVideoRef.srcObject = remoteStream;
+        //                     // remoteVideoRef.play();
+        //                 }
+        //             });
+        //         })
+        //         .catch((err) => {
+        //             console.log('Failed to get local stream', err);
+        //         });
+        // });
+    });
+});
